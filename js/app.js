072828a1d2a7207bf7bdb08abb079ba8d2fa7d47
celgutierrez <celgutierrez42@gmail.com>
2017-06-27 15:26:02 -0700

    addition of angular, removal of node

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('Porfolio', ['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlProvider',
+    function($stateProvider, $urlProvider) {
+        $urlProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html'
+            })
+            .state('projects', {
+                url: '/projects',
+                templateUrl: 'views/projects.html'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'views/contact.html'
+            })
+            .state('404', {
+                url: '/404',
+                templateUrl: 'views/404.html'
+            });
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+]);
